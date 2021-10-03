428c90d4224325729d5b09fe89521639ccfc54e9
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-03 23:31:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ int main() {
 
     for (int i = 0; i < 50; i++) {
         stack_push(&Stack, i);
-        printf("size: %5d, Capacity: %5d\n", Stack.size, Stack.capacity);
+        //printf("size: %5d, Capacity: %5d\n", Stack.size, Stack.capacity);
     }
 
-    Stack.ptr_begin = nullptr;
+    //Stack.ptr_begin[Stack.capacity - 1] = 100;
 
     for (int i = 0; i < 20; i++) {
         stack_pop(&Stack, 2);
-        printf("size: %5d, Capacity: %5d\n", Stack.size, Stack.capacity);
+        //printf("size: %5d, Capacity: %5d\n", Stack.size, Stack.capacity);
     }
 
     return 0;
