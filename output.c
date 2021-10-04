3c1263645489deed60899654b2cf084cada1ba09
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-04 04:47:43 +0300

    Update and rename output.cpp to output.c

--- output.c
+++ output.c
@@ -4,43 +4,32 @@
 #include "output.h"
 
 int STACK_IS_OK(stack_struct *cur_stack, char *path, int line, char *func_name) {
-    //{ # # # # # } - error code with any error
-    //1)size 2) capacity 3) ptr 4) hash 5) canary
-
     int special_code = 0;
     if (cur_stack->size < 0) {
-        ERROR_CODE = BAD_SIZE;
         special_code += 10000;
         printf(RED"\nBAD_SIZE. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
 
     if (cur_stack->capacity < 0) {
-        ERROR_CODE = BAD_CAPACITY;
         special_code += 1000;
         printf(RED"\nBAD_CAPACITY. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
 
     if (cur_stack->ptr_begin == nullptr) {
-        ERROR_CODE = BAD_PTR;
         special_code += 100;
         printf(RED"\nBAD_PTR. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
     if (cur_stack->hash != stack_hash_sum(cur_stack) ) {
-        ERROR_CODE = BAD_HASH;
         special_code += 10;
-        canary value = stack_hash_sum(cur_stack);
-        //printf("hash: %X. new value: %X", cur_stack->hash, value);
         printf(RED"\nBAD HASH. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
 
     if (cur_stack->canary_beg != cur_stack->ptr_begin[0]) {
-        ERROR_CODE = BAD_CANARY_BEGIN;
         special_code += 1;
         printf(RED"\nBAD_CANARY_BEGIN. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
 
     if (cur_stack->canary_end != cur_stack->ptr_begin[cur_stack->capacity - 1]) {
-        ERROR_CODE = BAD_CANARY_END;
         special_code += 100000;
         printf(RED"\nBAD_CANARY_END. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
@@ -61,7 +50,6 @@ void stack_dump(stack_struct *cur_stack, char *func_name, int line) {
     }
 
     else {
-
         printf("code: %d\n", code);
 
         printf(WHITE"%15s\t%-15s", "stack dump", "[ canary_end ]");
@@ -72,7 +60,6 @@ void stack_dump(stack_struct *cur_stack, char *func_name, int line) {
         }
 
         printf(WHITE"%15s\t%-15s", "stack dump", "[ Stack size ]");
-
         if (cur_stack->size < 0 || cur_stack->size > cur_stack->capacity) {
             printf(RED"%10s", "\t(ERROR)\n");
         } else {
