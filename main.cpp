12bfac2ecf6cbfeca6ae97e69fbf887c1a8a2211
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-04 13:27:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,16 @@ int main() {
 
     stack_ctor(&Stack);
 
-    for (int i = 0; i < 50; i++) {
+
+    for (int i = 0; i < 40; i++) {
         stack_push(&Stack, i);
     }
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 10; i++) {
         stack_pop(&Stack, 2);
     }
+
+    stack_dtor(&Stack);
 
     return 0;
 }
