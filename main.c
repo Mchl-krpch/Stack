0beaea902e134393571ca3910ca198db97ded41a
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-06 01:44:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,15 +4,14 @@
 int main() {
     Stack stack;
 
-    stack_ctor(&stack, 8);
-
+    stack_ctor(&stack, 2);
 
     for (int elem = 0; elem < 40; elem++) {
         stack_push(&stack, elem);
     }
 
     for (int elem = 0; elem < 10; elem++) {
-        stack_pop(&stack, 2);
+        stack_pop(&stack);
     }
 
     stack_dtor(&stack);
