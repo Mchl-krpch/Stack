9d51780e1c9296d1de71dada26b86629436c346d
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-06 01:46:16 +0300

    Update output.c

--- output.c
+++ output.c
@@ -3,96 +3,97 @@
 #include "stack.h"
 #include "output.h"
 
-int STACK_IS_OK(stack_struct *cur_stack, char *path, int line, char *func_name) {
+int stack_is_ok(Stack *stack, char *path, int line, const char *func_name) {
     int special_code = 0;
-    if (cur_stack->size < 0) {
-        special_code += 10000;
+    if (stack->size < 0) {
+        special_code += 1;
         printf(RED"\nBAD_SIZE. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
 
-    if (cur_stack->capacity < 0) {
-        special_code += 1000;
+    if (stack->capacity < 0) {
+        special_code += 1;
         printf(RED"\nBAD_CAPACITY. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
 
-    if (cur_stack->ptr_begin == nullptr) {
-        special_code += 100;
+    if (stack->data == nullptr) {
+        special_code += 1;
         printf(RED"\nBAD_PTR. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
-    if (cur_stack->hash != stack_hash_sum(cur_stack) ) {
-        special_code += 10;
+    if (stack->hash != stack_hash(stack) ) {
+        special_code += 1;
         printf(RED"\nBAD HASH. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
 
-    if (cur_stack->canary_beg != cur_stack->ptr_begin[0]) {
+    if (stack->canary_beg != stack->data[0]) {
         special_code += 1;
         printf(RED"\nBAD_CANARY_BEGIN. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
 
-    if (cur_stack->canary_end != cur_stack->ptr_begin[cur_stack->capacity - 1]) {
-        special_code += 100000;
+    if (stack->canary_end != stack->data[stack->capacity - 1]) {
+        special_code += 1;
         printf(RED"\nBAD_CANARY_END. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
 
     return special_code;
 }
 
-void stack_dump(stack_struct *cur_stack, char *func_name, int line) {
-    if (cur_stack->ptr_begin == nullptr) {
-        printf(RED"BAD STACK PTR: \t(ERROR)\n");
+void stack_dump(Stack *stack, const char *func_name, int line) {
+    if (stack->data == nullptr) {
+        printf(WHITE"%15s\t%-15s", "stack dump", "[ canary_end ]");
+        printf(RED"%10s", "(ERROR)\n");
         abort();
     }
 
-    int code = STACK_IS_OK(cur_stack, __FILE__, line, func_name);
+    int code = stack_is_ok(stack, __FILE__, line, func_name);
 
     if (code == 0) {
         return;
     }
 
     else {
-        printf("code: %d\n", code);
-
         printf(WHITE"%15s\t%-15s", "stack dump", "[ canary_end ]");
-        if (cur_stack->ptr_begin[cur_stack->capacity - 1] != cur_stack->canary_end) {
+        if (stack->data[stack->capacity - 1] != stack->canary_end) {
             printf(RED"%10s", "\t(ERROR)\n");
         } else {
             printf(GREEN"%10s", "(OK)\n");
         }
 
         printf(WHITE"%15s\t%-15s", "stack dump", "[ Stack size ]");
-        if (cur_stack->size < 0 || cur_stack->size > cur_stack->capacity) {
+        if (stack->size < 0 || stack->size > stack->capacity) {
             printf(RED"%10s", "\t(ERROR)\n");
         } else {
             printf(GREEN"%10s", "(OK)\n");
         }
 
         printf(WHITE"%15s\t%-15s", "stack dump", "[ capacity ]");
-        if (cur_stack->capacity < 0) {
+        if (stack->capacity < 0) {
             printf(RED"%10s", "\t(ERROR)\n");
         } else {
             printf(GREEN"%10s", "(OK)\n");
         }
 
-        printf(WHITE"%15s\t%-15s", "stack dump", "[ ptr_begin ]");
-        if (cur_stack->ptr_begin == nullptr) {
+        printf(WHITE"%15s\t%-15s", "stack dump", "[ data ]");
+        if (stack->data == nullptr) {
             printf(RED"%10s", "\t(ERROR)\n");
         } else {
             printf(GREEN"%10s", "(OK)\n");
         }
 
         printf(WHITE"%15s\t%-15s", "stack dump", "[ hash ]");
-        if (cur_stack->hash != stack_hash_sum(cur_stack)) {
+        if (stack->hash != stack_hash(stack)) {
             printf(RED"%10s", "\t(ERROR)\n");
         } else {
             printf(GREEN"%10s", "(OK)\n");
         }
 
         printf(WHITE"%15s\t%-15s", "stack dump", "[ canary_beg ]");
-        if (cur_stack->ptr_begin[0] != cur_stack->ptr_begin[0]) {
+        if (stack->data[0] != stack->data[0]) {
             printf(RED"%10s", "\t(ERROR)\n");
         } else {
             printf(GREEN"%10s", "(OK)\n");
         }
+
+        printf(RED"ERROR endng\n");
 
         abort();
     }
