1c8ce28709f640d1570844ddd0431e4e099063e2
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-06 18:45:45 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -5,19 +5,25 @@
 #include <cstdint>
 
 int const Canary = 0xB12D00;
+int const IncreaseMultiplier = 2;
+int const DecreaseMultiplier = 0.4;
+
+int const Toxic  = 0xBADD;
+int const Free   = 0xF2EE;
+
 typedef int sType;
 
 typedef  struct{
     int 	   canary_beg;  //begin bird
     int 	   capacity;    //max size
     int 	   size;        //cur number of element
-    int 	  *data;   //stack path
+    int 	  *data;        //stack path
     uint64_t   hash;    	//stack hash
     int 	   canary_end;	//end bird
 
 }Stack;
 
-int stack_ctor 		  (Stack *new_stack, int start_size		);
+int stack_ctor 		  (Stack *new_stack, int start_capacity );
 int stack_increase 	  (Stack *cur_stack                		);
 int stack_push 	      (Stack *cur_stack, sType value		);
 int stack_decrease 	  (Stack *cur_stack                		);
