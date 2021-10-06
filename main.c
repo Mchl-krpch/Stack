72c40d42cb8c943f20be824664205e583745865e
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-06 18:44:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
-#include "stack.h"
+#include <stdio.h>
 
+#include "stack.h"
 
 int main() {
     Stack stack;
 
     stack_ctor(&stack, 2);
 
-    for (int elem = 0; elem < 40; elem++) {
+    for (int elem = 0; elem < 100; elem++) {
         stack_push(&stack, elem);
+        printf("capacity: %d, size: %d\n", stack.capacity, stack.size);
     }
 
-    for (int elem = 0; elem < 10; elem++) {
+    for (int elem = 0; elem < stack.size; elem++) {
+        printf("%x\n", stack.data[elem]);
+    }
+
+
+    for (int elem = 0; elem < 80; elem++) {
         stack_pop(&stack);
+        printf("capacity: %d, size: %d\n", stack.capacity, stack.size);
     }
 
     stack_dtor(&stack);
