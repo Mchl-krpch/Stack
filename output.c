eb9aba5e6325278804b4027b96caa0b628d1e116
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-11 20:33:42 +0300

    Update output.c

--- output.c
+++ output.c
@@ -3,10 +3,20 @@
 #include "stack.h"
 #include "output.h"
 
+#if DEBUG_LVL == 2
+    #define DEBUG_LVL_2
+    #define DEBUG_LVL_1
+#else
+    #if DEBUG_LVL == 1
+        #define DEBUG_LVL_1
+    #endif
+#endif
+
+#ifdef DEBUG_LVL_1
 int stack_is_ok(Stack *stack, char *path, int line, const char *func_name) {
     int special_code = 0;
 
-    if (stack->canary_beg != Canary) {
+    if (stack->canary_beg != CANARY) {
         special_code += 1;
         printf(RED"\nBAD_CANARY_BEGIN. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
@@ -25,13 +35,16 @@ int stack_is_ok(Stack *stack, char *path, int line, const char *func_name) {
         special_code += 1;
         printf(RED"\nBAD_PTR. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
-    
-    if (stack->hash != stack_hash(stack) ) {
-        special_code += 1;
-        printf(RED"\nBAD HASH. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
-    }
 
-    if (stack->canary_end != Canary) {
+    #ifdef DEBUG_LVL_2
+        if (stack->hash != stack_hash(stack) ) {
+            special_code += 1;
+            printf(RED"\nBAD HASH. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
+        }
+    #endif //DEBUG_LVL_2
+
+    if (stack->canary_end != CANARY) {
+        printf("stack canary: %x const canary: %x", stack->canary_end, CANARY);
         special_code += 1;
         printf(RED"\nBAD_CANARY_END. FILE: %s\nfunction: %s(%d)\n", path, func_name, line);
     }
@@ -53,8 +66,8 @@ void stack_dump(Stack *stack, const char *func_name, int line) {
     }
 
     else {
-        printf(WHITE"%15s\t%-15s", "stack dump", "[ canary_end ]");
-        if (stack->data[stack->capacity - 1] != stack->canary_end) {
+        printf(WHITE"%15s\t%-15s", "stack dump", "[ canary_beg ]");
+        if (stack->canary_beg != CANARY) {
             printf(RED"%10s", "\t(ERROR)\n");
         } else {
             printf(GREEN"%10s", "(OK)\n");
@@ -81,15 +94,17 @@ void stack_dump(Stack *stack, const char *func_name, int line) {
             printf(GREEN"%10s", "(OK)\n");
         }
 
-        printf(WHITE"%15s\t%-15s", "stack dump", "[ hash ]");
-        if (stack->hash != stack_hash(stack)) {
-            printf(RED"%10s", "\t(ERROR)\n");
-        } else {
-            printf(GREEN"%10s", "(OK)\n");
-        }
+        #ifdef DEBUG_LVL_2
+            printf(WHITE"%15s\t%-15s", "stack dump", "[ hash ]");
+            if (stack->hash != stack_hash(stack)) {
+                printf(RED"%10s", "\t(ERROR)\n");
+            } else {
+                printf(GREEN"%10s", "(OK)\n");
+            }
+        #endif //DEBUG_LVL_2
 
-        printf(WHITE"%15s\t%-15s", "stack dump", "[ canary_beg ]");
-        if (stack->data[0] != stack->data[0]) {
+        printf(WHITE"%15s\t%-15s", "stack dump", "[ canary_end ]");
+        if (stack->canary_end != CANARY) {
             printf(RED"%10s", "\t(ERROR)\n");
         } else {
             printf(GREEN"%10s", "(OK)\n");
@@ -100,3 +115,4 @@ void stack_dump(Stack *stack, const char *func_name, int line) {
         abort();
     }
 }
+#endif //DEBUG_LVL_1
