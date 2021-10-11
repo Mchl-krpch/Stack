2765975ad440e01ca0f4ff0acc80c967510a238f
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-11 20:34:45 +0300

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 #include <cstdint>
-
-#include "assert.h"
+#include <assert.h>
 
 #include "stack.h"
 #include "output.h"
 
+#if DEBUG_LVL == 2
+    #define DEBUG_LVL_1
+    #define DEBUG_LVL_2
+    #define HASH stack->hash = stack_hash(stack);
+#else
+    #if DEBUG_LVL == 1
+        #define DEBUG_LVL_1
+        #define HASH
+    #else
+        #define HASH
+        #define stack_dump(stack, __func__, __LINE__);
+    #endif
+#endif
+
 int CODE = 0;
 
-int stack_ctor(Stack *stack, int start_capacity) {
+int stack_ctor(Stack *stack, size_t start_capacity) {
     assert(stack != nullptr && "stack_ctor: stack = nullptr");
 
     stack->size = 0;
     stack->capacity = start_capacity;
 
-    stack->canary_beg = Canary;
-    stack->canary_end = Canary;
+    #ifdef DEBUG_LVL_1
+        stack->canary_beg = CANARY;
+        stack->canary_end = CANARY;
+    #endif //DEBUG_LVL_1
 
     stack->data = (sType *)calloc(stack->capacity, sizeof(sType));
 
     if (stack->data == nullptr) {
         stack_dump(stack, __func__, __LINE__);
+        return 0;
     }
 
-    stack->hash = stack_hash(stack);
+    HASH
 
     return 0;
 }
@@ -34,35 +50,31 @@ int stack_push (Stack *stack, sType new_elem) {
     stack_dump(stack, __func__, __LINE__);
 
     if (stack->size >= stack->capacity) {
-        stack_increase(stack);
+        stack_change_cap(stack, stack->capacity * REALLOC_COEFF);
     }
 
-    else {
-        stack->data[stack->size] = new_elem;
-        stack->size++;
-    }
-    
-    stack->hash = stack_hash(stack);
+    stack->data[stack->size] = new_elem;
+    stack->size++;
+    HASH
+
 
     stack_dump(stack, __func__, __LINE__);
 
     return 0;
 }
 
-int stack_increase (Stack *stack) {
+int stack_change_cap(Stack *stack, size_t increase_by) {
     assert(stack != nullptr);
 
-    stack_dump(stack, __func__, __LINE__);
-
-
-    stack->capacity *= IncreaseMultiplier;
-    stack->data = (sType *) realloc(stack->data, stack->capacity * sizeof(sType));
+    stack->capacity = increase_by;
+    stack->data = (sType *) realloc(stack->data, increase_by * sizeof(sType));
 
     if (stack->data == nullptr) {
         stack_dump(stack, __func__, __LINE__);
+        return 0;
     }
 
-    stack->hash = stack_hash(stack);
+    HASH
 
     stack_dump(stack, __func__, __LINE__);
 
@@ -75,58 +87,15 @@ int stack_pop (Stack *stack) {
     stack_dump(stack, __func__, __LINE__);
 
     stack->size--;
-    stack->hash = stack_hash(stack);
+    HASH
 
-    if (stack->size <= (stack->capacity * 0.4) ) {
-        stack_decrease(stack);
-        stack->hash = stack_hash(stack);
+    if (stack->size <= (stack->capacity / REALLOC_COEFF / REALLOC_COEFF) ) {
+        stack_change_cap(stack, stack->capacity / REALLOC_COEFF);
     }
 
-    stack_dump(stack, __func__, __LINE__);
-
-    return 0;
-}
-
-int stack_decrease (Stack *stack) {
-    assert(stack != nullptr);
+    HASH
 
     stack_dump(stack, __func__, __LINE__);
-
-    stack->capacity /= IncreaseMultiplier;
-
-    stack->data = (int *) realloc(stack->data, stack->capacity * sizeof(int));
-
-    if (stack->data == nullptr) {
-        stack_dump(stack, __func__, __LINE__);
-    }
-
-    stack->hash = stack_hash(stack);
-
-    stack_dump(stack, __func__, __LINE__);
-
-
-    return 0;
-}
-
-int stack_shrink (Stack *stack) {
-    assert(stack != nullptr);
-
-    stack_dump(stack, __func__, __LINE__);
-
-    stack->data = (int *) realloc(stack->data, stack->size * sizeof(int));
-
-    stack->capacity = stack->size;
-
-    if (stack->data == nullptr) {
-        stack_dump(stack, __func__, __LINE__);
-    }
-
-    stack->data[stack->capacity - 1] = Canary;
-
-    stack->hash = stack_hash(stack);
-
-    stack_dump(stack, __func__, __LINE__);
-
 
     return 0;
 }
@@ -134,77 +103,36 @@ int stack_shrink (Stack *stack) {
 uint64_t stack_hash(Stack *stack) {
     assert(stack != NULL && "hash_sum: cur_stack = NULL");
 
-    int res = 0;
+    int hash = 0;
 
-    char *data = (char *)stack->data;
+    hash += hash_buffer((char *)stack->data, stack->capacity - 1);
+    hash += hash_buffer((char *)stack, sizeof(Stack) - 4 * sizeof(int));
 
-    for (int index = 0; index < stack->capacity - 1; index++) {
-        res += data[index] * index;
-    }
-
-    char *data2 = (char *)stack;
-    for (int index = 0; index < 4 * sizeof(int); index++) {
-        res += data2[index] * index;
-    }
-
-    //printf("hash: %d sizeof: %d", res, sizeof(Stack));
-
-    return res;
+    return hash;
 }
 
-uint64_t murmurhash(char *key, unsigned int len) {
-    const unsigned int mur = 0x5bd1e995;
-    const unsigned int special_code = 0;
-    const int r = 24;
+uint64_t hash_buffer(char *buffer, size_t size) {
+    int hash = 0;
 
-    unsigned int h = special_code ^ len;
-    const unsigned char *data = (const unsigned char *)key;
-
-    uint64_t k;
-    while (len >= 4) {
-        k = data[0];
-        k |= data[1] << 8;
-        k |= data[1] << 16;
-        k |= data[1] << 24;
-
-        k *= mur;
-        k ^= k >> r;
-        k *= mur;
-
-        h *= mur;
-        h ^=k;
-
-        data += 4;
-        len -=4;
+    for (int index = 0; index < size; index++) {
+        hash += buffer[index] * index;
     }
 
-    switch (len) {
-        case 3:
-            h ^= data[2] <<16;
-        case 2:
-            h ^= data[1] <<8;
-        case 1:
-            h ^= data[0];
-            h *= mur;
-
-    };
-
-    h ^= h >> 13;
-    h *= mur;
-    h ^= h >> 15;
-
-    return h;
+    return hash;
 }
 
 int stack_dtor(Stack *stack) {
     for (int elem = 0; elem < stack->capacity - 1; elem++) {
-        stack->data[elem] = Free;
+        stack->data[elem] = FREE;
     }
 
     stack->size       = 0;
     stack->capacity   = 0;
-    stack->canary_end = 0;
-    stack->canary_beg = 0;
+
+    #ifdef DEBUG_LVL_1
+        stack->canary_end = 0;
+        stack->canary_beg = 0;
+    #endif //DEBUG_LVL_1
 
     free(stack->data);
 
