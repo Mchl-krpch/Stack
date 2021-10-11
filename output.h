e39b87d6b6f6642124a57c5f1732525b0b61d5c7
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-11 20:34:14 +0300

    Update output.h

--- output.h
+++ output.h
@@ -1,15 +1,16 @@
 #ifndef STACK_2_OUTPUT_H
 #define STACK_2_OUTPUT_H
 
+
 #include "stack.h"
 
 #define GREEN  "\x1b[32m"
 #define RED    "\x1b[31m"
 #define WHITE  "\e[0;37m"
 
-
-int stack_is_ok(Stack *cur_stack, char *path, int line, const char *func_name);
-
-void stack_dump(Stack *cur_stack, const char *func_name, int line);
+#ifdef DEBUG_LVL_1
+	int stack_is_ok(Stack *cur_stack, char *path, int line, const char *func_name);
+	void stack_dump(Stack *cur_stack, const char *func_name, int line);
+#endif //DEBUG_LVL_1
 
 #endif //STACK_2_OUTPUT_HS
