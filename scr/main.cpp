1798942e405f9f90de2244718dafb26d91f56a13
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-12 23:26:22 +0300

    Update main.cpp

--- scr/main.cpp
+++ scr/main.cpp
@@ -7,16 +7,10 @@ int main() {
 
     stack_ctor(&stack, 8);
 
-
     for (int elem = 0; elem < 100; elem++) {
         stack_push(&stack, elem);
         printf("capacity: %d, size: %d, last elem: %d\n", stack.capacity, stack.size, stack.data[stack.size - 1]);
     }
-
-    stack_change_cap(&stack, stack.size);
-    stack.data[10] = 1000;
-    //stack.canary_end = 0xDDD;
-    //printf("canary beg: %x, canary end: %x\n", stack.canary_beg, stack.canary_end);
 
     for (int elem = 0; elem < 80; elem++) {
         stack_pop(&stack);
