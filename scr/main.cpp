a956a24876405bdf0f9c7718c6a1d860d25a280a
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-24 02:37:19 +0300

    Update main.cpp

--- scr/main.cpp
+++ scr/main.cpp
@@ -2,38 +2,44 @@
 
 #include "stack.h"
 
+char name[60];
+
 int main() {
     Stack stack;
+    int code = 0;
+    sType top_element;
 
-    stack_ctor(&stack, 8);
+    #if LOG_INFO == 1
+        create_log_file(name);
+    #endif
+
+    code = stack_ctor(&stack, 8);
+    if (code != NO_ERRORS) {
+        return -1;
+    }
 
     for (int elem = 0; elem < 10; elem++) {
-        stack_push(&stack, elem);
+        code = stack_push(&stack, elem);
         printf("capacity: %d, size: %d, last elem: %d\n", stack.capacity, stack.size, stack.data[stack.size - 1]);
     }
 
-    /* stack hash check
-    stack.data[13] = 0;
-    */
-
-    /* stack canary check
-    stack.canary_beg = 0;
-    */
-
-    /* stack size check
-    stack.size = -1;
-    */
-
-    /* stack size check
-    stack.capacity = -1;
-    */
+    code = stack_top(&stack, &top_element);
+    if (code != NO_ERRORS) {
+        return -1;
+    }
 
     for (int elem = 0; elem < 5; elem++) {
-        stack_pop(&stack);
+        code = stack_pop(&stack);
+        if (code != NO_ERRORS) {
+        return -1;
+        }
         printf("capacity: %d, size: %d\n", stack.capacity, stack.size);
     }
 
-    stack_dtor(&stack);
+    code = stack_dtor(&stack);
+    if (code != NO_ERRORS) {
+        return -1;
+    }
 
     return 0;
 }
