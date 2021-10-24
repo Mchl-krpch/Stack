54b3ff30ec92aeba8a204c7f6221c08d736b2c48
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-10-25 02:26:17 +0300

    Update main.cpp

--- scr/main.cpp
+++ scr/main.cpp
@@ -2,38 +2,30 @@
 
 #include "stack.h"
 
-#if DEBUG_LVL == 2
-char name[60] = "logfile";
-#else
-char name[60];
-#endif
-
 int main() {
     Stack stack;
     int code = 0;
     sType top_element;
 
-    code = stack_ctor(&stack, 8, name);
+    code = stack_ctor(&stack, 8);
     if (code != NO_ERRORS) {
         return -1;
     }
 
     for (int elem = 0; elem < 10; elem++) {
         code = stack_push(&stack, elem);
-        printf("capacity: %d, size: %d, last elem: %d\n", stack.capacity, stack.size, stack.data[stack.size - 1]);
     }
 
     code = stack_top(&stack, &top_element);
     if (code != NO_ERRORS) {
         return -1;
     }
-
+    
     for (int elem = 0; elem < 5; elem++) {
         code = stack_pop(&stack);
         if (code != NO_ERRORS) {
         return -1;
         }
-        printf("capacity: %d, size: %d\n", stack.capacity, stack.size);
     }
 
     code = stack_dtor(&stack);
